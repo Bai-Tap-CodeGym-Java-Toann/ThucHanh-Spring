fc10a9d8733170231aabcd7b89336c22b9a58ce4
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-10-24 11:40:51 +0700

    fin.

--- src/main/java/controllers/CurencyController.java
+++ src/main/java/controllers/CurencyController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,10 @@ public class CurencyController {
     }
 
     @PostMapping
-    private ModelAndView convert(@RequestParam String money, String rate) {
+    private ModelAndView convert(String money, String rate) {
         try {
-            Double.parseDouble(money);
             Double.parseDouble(rate);
+            Double.parseDouble(money);
         } catch (Throwable a) {
             return new ModelAndView("error404");
         }
